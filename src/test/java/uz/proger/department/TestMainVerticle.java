4efa212be22634530644f273e979b773893bb8ce
unknown <azizxonmamurovich202@gmail.com>
2021-05-18 06:45:09 -0700

    new

--- src/test/java/uz/proger/department/TestMainVerticle.java
+++ src/test/java/uz/proger/department/TestMainVerticle.java
@@ -3,6 +3,9 @@ package uz.proger.department;
 import io.vertx.core.Vertx;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
+
+import com.safenetpay.department.MainVerticle;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
